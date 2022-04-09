0ae096d5c59cfe0fb71f4c8da7b36b49157a6f21
Guto Xavier <gutoffline@gmail.com>
2022-04-09 13:52:55 -0300

    atividade

--- 002/script.js
+++ 002/script.js
@@ -1,7 +1,3 @@
-
 function exibir(n1 = 0) {
-    console.log("Informe um número")
-    console.log("O número informado foi ", n1)
+    document.querySelector("#resultado").innerHTML = "O número informado foi " + n1
 }
-
-exibir(8)
--- 017/script.js
+++ 017/script.js
@@ -1,22 +1,13 @@
 // Faça um Programa que peça dois números e imprima o maior deles.
-function maiorNumero(n1=0, n2=0, n3=0){
-    if(n1 > n2 && n1 > n3){
-        console.log("O maior número é", n1)
-    }else if(n2 > n1 && n2 > n3){
-        console.log("O maior número é", n2)
-    }else if(n3 > n1 && n3 > n2){
-        console.log("O maior número é", n3)
+function maiorNumero(n1 = 0, n2 = 0, n3 = 0) {
+    n1 = parseFloat(n1)
+    n2 = parseFloat(n2)
+    n3 = parseFloat(n3)
+    if (n1 > n2 && n1 > n3) {
+        document.querySelector("#resultado").innerHTML = "O maior número é " + n1
+    } else if (n2 > n1 && n2 > n3) {
+        document.querySelector("#resultado").innerHTML = "O maior número é " + n2
+    } else if (n3 > n1 && n3 > n2) {
+        document.querySelector("#resultado").innerHTML = "O maior número é " + n3
     }
 }
-
-maiorNumero(7,3,10)
-
-
-function soma(n1=0 , n2=0){
-    let total
-    total = n1 + n2
-    console.log(total)
-}
-
-soma(2,3)
-
